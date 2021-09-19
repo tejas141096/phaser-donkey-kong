3589a3a4b2ddcca002c79299658d3c659f1cf611
tejas141096 <tejas141096@gmail.com>
2021-09-19 15:22:03 -0600

    scene logic added

--- dk_game/js/SceneHandler.js
+++ dk_game/js/SceneHandler.js
@@ -3,13 +3,16 @@
  */
 
 // Scene Handler. Main File to call Scenes
-const Phaser = require("./phaser");
+// var config = {
+//     // type: Phaser.AUTO,
+//     width: 360,
+//     height: 590,
+//     state: [MainGame]
+// };
 
-var config = {
-    type: Phaser.AUTO,
-    width: 360,
-    height: 590,
-    scene: [MainGame]
-};
+// var game = new Phaser.Game(config);
 
-var game = new Phaser.Game(config);
+var game = new Phaser.Game(360, 592, Phaser.AUTO);
+
+game.state.add('MainGame', MainGame);
+game.state.start('MainGame');
