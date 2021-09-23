13f1cfda0171c92301c471023e6b91b7ee4e05ea
tejas141096 <tejas141096@gmail.com>
2021-09-23 02:41:28 -0600

    start menu

--- dk_game/js/SceneHandler.js
+++ dk_game/js/SceneHandler.js
@@ -14,8 +14,8 @@
 
 var game = new Phaser.Game(800, 960, Phaser.AUTO);
 
-// game.state.add("Boot", boot);
-// game.state.add("Loading", loading);
-// game.state.add("GameTitle", gameTitle);
+game.state.add("Boot", boot);
+game.state.add("Loading", loading);
+game.state.add("MainMenu", MainMenu);
 game.state.add("MainGame", MainGame);
-game.state.start("MainGame");
+game.state.start("Boot");
